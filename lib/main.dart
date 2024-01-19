baf77d72eb71dffd1c8aa43d7668b1c61f04e40c
tcotidiane33 <tcotidiane33@gmail.com>
2024-01-19 18:49:54 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_foodybite/screens/home.dart';
+import 'package:flutter_foodybite/screens/categories.dart'; // Importez vos autres écrans ici
+import 'package:flutter_foodybite/screens/label.dart';
+import 'package:flutter_foodybite/screens/notifications.dart';
+import 'package:flutter_foodybite/screens/profile.dart';
+import 'package:flutter_foodybite/util/const.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: Constants.appName,
+      theme: Constants.lightTheme,
+      darkTheme: Constants.darkTheme,
+      home: Home(),
+      routes: {
+        '/categories': (context) => Categories(),
+        '/label': (context) => Label(),
+        '/notifications': (context) => Notifications(),
+        '/profile': (context) => Profile(),
+      },
+    );
+  }
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,237 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_foodybite/screens/categories.dart';
+import 'package:flutter_foodybite/screens/trending.dart';
+import 'package:flutter_foodybite/util/categories.dart';
+import 'package:flutter_foodybite/util/friends.dart';
+import 'package:flutter_foodybite/util/restaurants.dart';
+import 'package:flutter_foodybite/widgets/category_item.dart';
+import 'package:flutter_foodybite/widgets/search_card.dart';
+import 'package:flutter_foodybite/widgets/slide_item.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+
+  final List<Widget> _children = [
+    HomeContent(),
+    Categories(),
+    Trending(),
+    // Ajoutez vos autres écrans ici
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: onTabTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.trending_up),
+            label: 'Trending',
+          ),
+          // Ajoutez vos autres éléments ici
+        ],
+      ),
+    );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+}
+
+class HomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
+        child: ListView(
+          children: <Widget>[
+            buildSearchBar(context),
+            SizedBox(height: 20.0),
+            buildRestaurantRow('Nouvelles Annonces', context),
+            SizedBox(height: 10.0),
+            buildRestaurantList(context),
+            SizedBox(height: 10.0),
+            buildCategoryRow('Categories', context),
+            SizedBox(height: 10.0),
+            buildCategoryList(context),
+            SizedBox(height: 20.0),
+            buildCategoryRow('Friends', context),
+            SizedBox(height: 10.0),
+            buildFriendsList(),
+            SizedBox(height: 30.0),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+  buildRestaurantRow(String restaurant, BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(
+          "$restaurant",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+        TextButton(
+          child: Text(
+            "See all (19)",
+            style: TextStyle(
+              color: Colors.blue, // Set your desired color here
+            ),
+          ),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (BuildContext context) {
+                  return Trending();
+                },
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  buildCategoryRow(String category, BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(
+          "$category",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+        TextButton(
+          child: Text(
+            "See all (12)",
+            style: TextStyle(
+              color: Colors.blue, // Set your desired color here
+            ),
+          ),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (BuildContext context) {
+                  return Categories();
+                },
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  buildSearchBar(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(10, 5, 10, 0),
+      child: SearchCard(),
+    );
+  }
+
+  buildCategoryList(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height / 6,
+      child: ListView.builder(
+        primary: false,
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        itemCount: categories == null ? 0 : categories.length,
+        itemBuilder: (BuildContext context, int index) {
+          Map cat = categories[index];
+
+          return CategoryItem(
+            cat: cat,
+          );
+        },
+      ),
+    );
+  }
+
+  buildRestaurantList(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height / 2.4,
+      width: MediaQuery.of(context).size.width,
+      child: ListView.builder(
+        primary: false,
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        itemCount: restaurants == null ? 0 : restaurants.length,
+        itemBuilder: (BuildContext context, int index) {
+          Map restaurant = restaurants[index];
+
+          return Padding(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: SlideItem(
+              img: restaurant["img"],
+              title: restaurant["title"],
+              address: restaurant["address"],
+              rating: restaurant["rating"],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  buildFriendsList() {
+    return Container(
+      height: 50.0,
+      child: ListView.builder(
+        primary: false,
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        itemCount: friends == null ? 0 : friends.length,
+        itemBuilder: (BuildContext context, int index) {
+          String img = friends[index];
+
+          return Padding(
+            padding: const EdgeInsets.only(right: 5.0),
+            child: CircleAvatar(
+              backgroundImage: AssetImage(
+                img,
+              ),
+              radius: 25.0,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
